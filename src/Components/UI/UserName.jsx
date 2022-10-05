0f6f20d69a8dd2d1dfa1fe322f5a2c2806cdbee3
Kamchybekk <manasbekoovich@gmail.com>
2022-10-06 02:54:15 +0600

    delete notification item

---
+++ src/Components/UI/UserName.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const UserName = () => {
+  return <StyledUserName>Nazira Nazirova</StyledUserName>;
+};
+
+export default UserName;
+
+const StyledUserName = styled.h2`
+  font-weight: 500;
+`;
