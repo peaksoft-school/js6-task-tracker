afab0ee641bda7bcea1fbd6948f0c7865b5bcf4f
Kamchybekk <manasbekoovich@gmail.com>
2022-10-14 17:02:21 +0600

    create progress bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react"
+import ProgressBar from "./Components/UI/ProgressBar"
 
 function App() {
-   return <div className="App">test</div>
+   return (
+      <div className="App">
+         <ProgressBar widthProgressPercent="99" tasks="10" completedTasks="3" />
+      </div>
+   )
 }
 
 export default App
