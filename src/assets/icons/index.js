16f559e990f622467d17c7382701f871595cc192
Tima Kyrgyz <109202799+KatsuTima@users.noreply.github.com>
2022-10-17 19:47:26 +0300

    Finish Text Area

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,2 +1,4 @@
 export { ReactComponent as HidePassword } from "./HidePassword.svg"
 export { ReactComponent as ShowPassword } from "./ShowPassword.svg"
+export { ReactComponent as NotifyIcon } from "./@.svg"
+export { ReactComponent as ClockIcon } from "./clock.svg"
