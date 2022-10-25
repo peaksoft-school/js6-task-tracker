62954a8eb55d2138fae5893519ab32b90bd5ee69
Daniel <zadrot105217@gmail.com>
2022-10-25 12:07:08 +0600

    feature/MemberBoard is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from "react"
-import MemberBoard from "./Components/MemberBoard"
-import Ava from "./assets/svg/Frame 15 (1).svg"
 
 function App() {
-   return (
-      <div className="App">
-         <MemberBoard titleBoard="LMS" discription="System" boardIcon={Ava} />
-      </div>
-   )
+   return <div className="App">test</div>
 }
 
 export default App
