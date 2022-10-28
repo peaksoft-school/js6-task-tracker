fed2b6141807561b85bbc38059684b61372c7ef2
Daniel <zadrot105217@gmail.com>
2022-10-28 11:10:30 +0600

    feature/CreateBoard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from "react"
-import CreateBoard from "./Components/CreateBoard"
-// import Icon from "./assets/svg/defaultAvatar.svg"
 
 function App() {
-   return (
-      <div className="App">
-         <CreateBoard />
-      </div>
-   )
+   return <div className="App">test </div>
 }
 
 export default App
