64123e53d859cbfdfe1375fc281649ee66c63249
Kamchybekk <manasbekoovich@gmail.com>
2022-10-29 15:25:28 +0600

    delete button logout in admin add in user layout

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -1,12 +1,21 @@
 import React from "react"
+import { useDispatch } from "react-redux"
 import Header from "../Components/Header"
 import SideBar from "./SideBar/SideBar"
+import { logout } from "../store/AuthSlice"
 
 const UserLayout = () => {
+   const dispatch = useDispatch()
+   const logoutHandler = () => {
+      dispatch(logout())
+   }
    return (
       <>
          <Header />
          <SideBar />
+         <button type="submit" onClick={logoutHandler}>
+            LOGOUT
+         </button>
       </>
    )
 }
--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,20 +1,11 @@
 import React from "react"
-import { useDispatch } from "react-redux"
 import UserLayout from "../layout/UserLayout"
-import { logout } from "../store/AuthSlice"
 
 const AdminRoutes = () => {
-   const dispatch = useDispatch()
-   const logoutHandler = () => {
-      dispatch(logout())
-   }
    return (
       <div>
          <UserLayout />
          <h1>ADMIN</h1>
-         <button type="submit" onClick={logoutHandler}>
-            LOGOUT
-         </button>
       </div>
    )
 }
