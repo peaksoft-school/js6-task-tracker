b5345d808c7e22e7d1de9b9c96cdc3ae47c72dff
Tima Kyrgyz <109202799+KatsuTima@users.noreply.github.com>
2022-10-31 13:20:48 +0300

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import React, { useState } from "react"
-import CheckBox from "./Components/UI/CheckBox"
+import React from "react"
 
 function App() {
-   const [checked, setCheked] = useState(true)
-
-   const handleChange = (event) => {
-      setCheked(event.target.checked)
-   }
-   return (
-      <div>
-         <CheckBox checked={checked} onChange={handleChange} />
-      </div>
-   )
+   return <div>test</div>
 }
 
 export default App
