672d985be020d480a5948279cfe4f0f78d8e6bd8
Tima Kyrgyz <109202799+KatsuTima@users.noreply.github.com>
2022-10-31 12:12:46 +0300

    Clear App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from "react"
-import FavouritesWallpaper from "./Components/UI/FavouritesWallpaper"
 
 function App() {
-   const listBoard = [
-      { id: 1, titleCard: "Title", nameBoard: "TimaTop" },
-      { id: 2, titleCard: "Titsadasdale", nameBoard: "TimaTop" },
-      { id: 3, titleCard: "Ti123tle", nameBoard: "TimaTop" },
-      { id: 4, titleCard: "Tibvbvbvbvbtle", nameBoard: "TimaTop" },
-   ]
-   return (
-      <div>
-         <FavouritesWallpaper listBoard={listBoard} />
-      </div>
-   )
+   return <div>test</div>
 }
 
 export default App
