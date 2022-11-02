e9e9bc1dd52247096a0a08b9f8b107ba98f7c513
Daniel <zadrot105217@gmail.com>
2022-11-02 12:31:10 +0600

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react"
-import CreateBoard from "./Components/CreateBoard"
 
 function App() {
-   return (
-      <div className="App">
-         <CreateBoard />
-      </div>
-   )
+   return <div className="App">test</div>
 }
 
 export default App
