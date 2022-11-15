5d11ef76200a9c12ddeb92eb266771591c2d55ea
Kamchybekk <manasbekoovich@gmail.com>
2022-11-15 15:23:25 +0600

    save

---
+++ src/layout/UserAdminLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../Components/Header"
+import MemberBoard from "../Components/MemberBoard"
+
+const UserAdminLayout = () => {
+   return (
+      <>
+         <Header />
+         <MemberBoard />
+      </>
+   )
+}
+
+export default UserAdminLayout
