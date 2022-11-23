5063d913828283e77436b1a4f6f5008de333e6c8
Daniel <zadrot105217@gmail.com>
2022-11-23 12:47:16 +0600

    resolve conflict

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux"
 import App from "./App"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import { store } from "./store"
+import { store } from "./store/index"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
